a4324a3c8317bc74815f995b04041694a1447919
John Eric Marasigan <maryjoyce.llabres@g.batstate-u.edu.ph>
2022-08-28 21:12:44 +0800

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,5 +1,5 @@
 import os
 
-DB_USERNAME = os.environ("DB_USERNAME", "")
-DB_PASSWORD = os.environ("DB_PASSWORD", "")
-DB_CLUSTER_CODE = os.environ("DB_CLUSTER_CODE", "")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_CLUSTER_CODE = os.getenv("DB_CLUSTER_CODE")
