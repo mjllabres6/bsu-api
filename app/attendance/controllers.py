d4c1ec03ebb86169918aed69480111bb256160f9
John Eric Marasigan <maryjoyce.llabres@g.batstate-u.edu.ph>
2022-12-06 00:39:28 +0800

    Update controllers.py

--- app/attendance/controllers.py
+++ app/attendance/controllers.py
@@ -46,7 +46,8 @@ class AttendanceManager(object):
     def register_attendance(cls, code, body):
         from app import db
         import pytz
-
+        print(code)
+        print({"sr_code": code})
         student = db.students.find_one({"sr_code": code})
 
         current_class = db.classes.find_one({"code": code})
